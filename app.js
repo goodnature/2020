3f4853ea14ccca9a0c96d837e55a5acac6fe2e22
Daryosh <daryoosh.ka@gmail.com>
2021-01-04 12:44:34 +1100

    new index

---
+++ app.js
@@ -0,0 +1,56 @@
+const aspect = 5376/3360
+const width = 700
+
+const root = document.getElementById('eyes')
+
+const bg = document.createElement('img')
+root.appendChild(bg)
+bg.src = 'blacklogo.png'
+bg.style.width = width + 'px'
+bg.style.height = (width/aspect)+'px'
+bg.style.position = 'absolute'
+
+const pupil1 = document.createElement('img')
+root.appendChild(pupil1)
+pupil1.src = 'pupil1.png'
+pupil1.style.width = width+'px'
+pupil1.style.height = (width/aspect)+'px'
+pupil1.style.position = 'absolute'
+
+const pupil2 = document.createElement('img')
+root.appendChild(pupil2)
+pupil2.src = 'pupil2.png'
+pupil2.style.width = width+'px'
+pupil2.style.height = (width/aspect)+'px'
+pupil2.style.position = 'absolute'
+
+
+document.addEventListener('mousemove', function (e) {
+  {
+    const x = 402
+    const y = 192
+
+    let dx = e.clientX - x
+    let dy = e.clientY - y
+    let n = 0.1*Math.sqrt(dx*dx + dy*dy)
+    let bx = Math.abs(dx/n)
+    let by = Math.abs(dy/n)
+    dx = Math.min(bx, Math.max(-bx, dx))
+    dy = Math.min(by, Math.max(-by, dy))
+    pupil1.style.transform = 'translate(' + dx + 'px, ' + dy + 'px)'
+  }
+
+  {
+    const x = 352
+    const y = 192
+
+    let dx = e.clientX - x
+    let dy = e.clientY - y
+    let n = 0.1*Math.sqrt(dx*dx + dy*dy)
+    let bx = Math.abs(dx/n)
+    let by = Math.abs(dy/n)
+    dx = Math.min(bx, Math.max(-bx, dx))
+    dy = Math.min(by, Math.max(-by, dy))
+    pupil2.style.transform = 'translate(' + dx + 'px, ' + dy + 'px)'
+  }
+})
